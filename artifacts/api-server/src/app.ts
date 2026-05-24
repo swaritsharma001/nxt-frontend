ffbaea73ebe2f688bef9fc2aa9850413bc4c7ac5
UJJWAL SHARMA <152766614+swaritsharma001@users.noreply.github.com>
2026-05-24 04:17:37 +0000

    hshs

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,23 +1,23 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp, { HttpLogger } from "pino-http";
+import * as pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
 app.use(
-  pinoHttp({
+  (pinoHttp as any).default({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,10 +25,10 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api", router);
 
 export default app;
